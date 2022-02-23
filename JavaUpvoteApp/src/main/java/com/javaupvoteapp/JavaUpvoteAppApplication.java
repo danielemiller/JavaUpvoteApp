7b1db9b673452ac4ccfe99dfb413d92add9995ac
Daniel Miller <danielemiller@live.com>
2022-02-22 23:10:12 -0500

    initial check-in of the JavaUpvoteApp code

---
+++ JavaUpvoteApp/src/main/java/com/javaupvoteapp/JavaUpvoteAppApplication.java
@@ -0,0 +1,13 @@
+package com.javaupvoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUpvoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUpvoteAppApplication.class, args);
+	}
+
+}
---
+++ JavaUpvoteApp/src/test/java/com/javaupvoteapp/JavaUpvoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaupvoteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUpvoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
